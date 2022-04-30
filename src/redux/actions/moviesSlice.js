9c5cbc797cd856d04c2406ddab3e17c7902888a5
acf77 <antoniocarlos.j77@gmail.com>
2022-04-30 00:06:39 -0300

    fix(env file): add api key to env file
    added the api key from TMDB to a local env file

--- src/redux/actions/moviesSlice.js
+++ src/redux/actions/moviesSlice.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const getMoviesList = createAsyncThunk("movies/getMovies", async () => {
   const { data } = await axios.get(
-    "https://api.themoviedb.org/3/discover/movie?release_date=2022-01-01&api_key=54ffed57deb5a7a8688be4de3007e578"
+    `https://api.themoviedb.org/3/discover/movie?release_date=2022-01-01&api_key=${process.env.REACT_APP_TMDB_API_KEY}`
   );
   return data.results;
 });
